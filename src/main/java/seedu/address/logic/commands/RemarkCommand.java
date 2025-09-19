3be7e1c9b71f757f0ee0049027f76aea46dce49a
Andre Keh <andrekeh@gmail.com>
2025-09-20 01:52:30 +0800

    Change success message

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -26,7 +26,7 @@ public class RemarkCommand extends Command {
             + "Existing remark will be overwritten by the input.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "r/ [REMARK]\n"
-            + "Example: " + COMMAND_WORD + " 1 "
+            + "Examples: " + COMMAND_WORD + " 1 "
             + "r/ Likes to swim.";
 
     private final Index index;
